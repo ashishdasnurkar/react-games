25892acc6bf4c30c38b07834d4bcaff98b4ff54e
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2019-08-13 22:06:53 +1000

    class btn comp

--- components.js
+++ components.js
@@ -1,8 +1,15 @@
 console.log("All good");
 var jsContainer = document.getElementById("mountNode");
+var jsContainer2 = document.getElementById("mountNode2");
 
 function Button (props) {
   return <button type="submit">{props.label}</button>;
 }
 
-ReactDOM.render(<Button label="Test" />, jsContainer);
+class Button2 extends React.Component {
+  render() {
+    return <button> {this.props.label}</button>
+  }
+}
+ReactDOM.render(<Button label="Functional Button" />, jsContainer);
+ReactDOM.render(<Button2 label="Classy Button" />, jsContainer2);
