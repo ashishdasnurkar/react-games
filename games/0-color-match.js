9d08daaadda9706ff86330529b7c5900349cd360
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2019-08-20 20:38:21 +1000

    added game status state

--- games/0-color-match.js
+++ games/0-color-match.js
@@ -1,7 +1,17 @@
 console.log("All good");
 var jsContainer = document.getElementById("mountNode");
-
+const colors = [
+  'black', 'blue', 'green', 'yellow', 'red'
+]
+console.log(_.sample(colors));
 class Game extends React.Component {
+  state = {
+    meaningWord: _.sample(colors), 
+    inkWord: _.sample(colors), 
+    inkColor: _.sample(colors),
+    gameStatus: 'wrong'
+  };
+  
   render() {
     return (
       <div className="game">
@@ -10,9 +20,11 @@ class Game extends React.Component {
           color of the bottom word?
         </div>
         <div className="body">
-          <div className="game-status status-playing" />
-          <div className="meaning">Meaning</div>
-          <div className="ink">Ink</div>
+          <div className= {
+            `game-status status-${this.state.gameStatus}`
+          } />
+          <div className="meaning">{this.state.meaningWord}</div>
+          <div className="ink" style={{color: this.state.inkColor}}>{this.state.inkWord}</div>
           <div className="buttons">
             <button>YES</button>
             <button>NO</button>
